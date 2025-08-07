a5f4aefeeabbab5c1d32647875377ca7043d4685
HwangHotae <hotae0321@naver.com>
2025-08-07 19:36:44 +0900

    modify bug

--- src/component/HoldCard.jsx
+++ src/component/HoldCard.jsx
@@ -10,16 +10,14 @@ const HoldCard = ({ cardData }) => {
       {cardData.length === 0 ? <div>새로운 카드를 등록해주세요</div> : null}
 
       <div>
-        {cardData.map((item) => (
-          <div className="list_wrapper">
-            {cardData.map((item) => (
-              <div key={item.id} className="card-item">
-                <Card {...item} />
-                <button className="btn-payment">이 카드로 결재하기</button>
-              </div>
-            ))}
-          </div>
-        ))}
+        <div className="list_wrapper">
+          {cardData.map((item) => (
+            <div key={item.id} className="card-item">
+              <Card {...item} />
+              <button className="btn-payment">이 카드로 결재하기</button>
+            </div>
+          ))}
+        </div>
       </div>
 
       <div className="CardAdd" onClick={() => nav("/cardinfo")}>
