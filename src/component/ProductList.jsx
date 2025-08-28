4806b659d17fe64dae76fcc60283bad8e77ba3e8
HwangHotae <hotae0321@naver.com>
2025-08-28 21:45:35 +0900

    CSS MOBILE UPDATE

--- src/component/ProductList.jsx
+++ src/component/ProductList.jsx
@@ -3,12 +3,14 @@ import ProductCard from "./ProductCard";
 
 const ProductList = ({ products }) => {
   return (
-    <div className="product-list">
-      {products.map((product) => (
-        <div key={product.id}>
-          <ProductCard {...product} />
-        </div>
-      ))}
+    <div className="productList">
+      <div className="productListWrap">
+        {products.map((product) => (
+          <div key={product.id}>
+            <ProductCard {...product} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
